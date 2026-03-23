6247dc6abf0df43a6ea3e23dd83683baba9f9f5f
PUREsoft-mainal <manblazman@gmail.com>
2026-03-23 21:20:24 +0200

    Update scanner.py

--- src/engine/scanner.py
+++ src/engine/scanner.py
@@ -1,4 +1,15 @@
-﻿# أضف هذه الدوال داخل كلاس SmartScanner السابق:
+import os
+import sys
+
+# الحصول على المسار الحقيقي للمجلد الذي يوجد فيه الملف الآن
+BASE_DIR = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+def get_path(relative_path):
+    return os.path.join(BASE_DIR, relative_path)
+
+# الآن بدلاً من كتابة 'config/config.json' سنكتب:
+# get_path('config/config.json')
+# أضف هذه الدوال داخل كلاس SmartScanner السابق:
 
     def add_single_file(self, file_path):
         name = os.path.basename(file_path)
