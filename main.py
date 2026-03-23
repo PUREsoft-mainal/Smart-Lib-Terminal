ec85f69c0c192a35480b6c1e02be3e043692ec07
PUREsoft-mainal <manblazman@gmail.com>
2026-03-23 21:22:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ from src.engine.analyzer import ProjectAnalyzer
 from src.ui.main_terminal import main_loop
 from watchdog.observers import Observer
 
+# الحصول على المسار الحقيقي للمجلد الذي يوجد فيه الملف الآن
+BASE_DIR = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+def get_path(relative_path):
+    return os.path.join(BASE_DIR, relative_path)
+
+# الآن بدلاً من كتابة 'config/config.json' سنكتب:
+# get_path('config/config.json')
+
 def run_background_services():
     """تشغيل مراقب النظام (الصياد) ومراقب المشاريع (الحاقن)"""
     scanner = SmartScanner()
